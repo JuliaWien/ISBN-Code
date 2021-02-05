605dbdb07882a178c856eae7a85f74666bd8a0b7
unknown <julia.wienkop@gmail.com>
2021-02-05 18:46:48 +0100

    fullfils task b

--- main.js
+++ main.js
@@ -1,13 +1,81 @@
-const {checkISBN} = require('./src/ISBN_Code')
+//const { validate } = require('webpack')
+const { checkISBN } = require('./src/ISBN_Code')
+//const {berechne} = require('./src/ISBN_Code')
 
-const isbn = document.querySelector('#count').value
-
-function berechne()
-{
-    alert("Hallo Ansbach");
-     document.querySelector('#result').innerHTML=  checkISBN(isbn)
+function clear() {
+    document.getElementById('validText').style.visibility = 'hidden';
+    document.getElementById('invalidText').style.visibility = 'hidden';
 }
 
-const button = document.querySelector('#calc')
-button.addEventListener('click', berechne);
-//document.querySelector('#result').innerHTML = isbn(45734857392)
+function getInput() {
+    return document.querySelector('#count').value;
+}
+
+function showValid() {
+    document.getElementById('validText').style.visibility = 'visible';
+}
+
+function showInvalid() {
+    document.getElementById('invalidText').style.visibility = 'visible';
+}
+
+function debug() {
+    document.getElementById('debug').innerHTML = "Hello Test";
+}
+
+function getISBN3() {
+    return document.getElementById('isbn1').value + "-" +
+        document.getElementById('isbn2').value + "-" +
+        document.getElementById('isbn3').value;
+}
+function getISBN4() {
+    return document.getElementById('isbn1').value + "-" +
+        document.getElementById('isbn2').value + "-" +
+        document.getElementById('isbn3').value + "-" +
+        document.getElementById('isbn4').value;
+}
+
+function berechne() { 
+    clear();
+    const isbn = document.getElementById('isbn').value;
+    const pruefziffer = checkISBN(isbn);
+    if (pruefziffer === isbn[isbn.length - 1]) {
+        document.getElementById('validText').style.visibility = 'visible';
+    }
+    else 
+        document.getElementById('invalidText').style.visibility = 'visible';
+}
+
+function ISBNTest(e) {
+    clear();
+    berechne();
+
+    var target = e.target;
+    if (target.value.length >= target.attributes["maxlength"].value) {
+        let next = target;
+        while (next = next.nextElementSibling) {
+            if (next == null)
+                break;
+            if (next.tagName.toLowerCase() == "input") {
+                next.focus();
+                break;
+            }
+        }
+    } else if (target.value.length == 0) {
+        var previous = target;
+        while (previous = previous.previousElementSibling) {
+            if (previous == null)
+                break;
+            if (previous.tagName.toLowerCase() === "input") {
+                previous.focus();
+                break;
+            }
+        }
+    }
+}
+
+clear();
+
+
+document.getElementById('calc').addEventListener("click", berechne);
+document.querySelector('#isbn').addEventListener('input', ISBNTest);
--- src/ISBN_Code.test.js
+++ src/ISBN_Code.test.js
@@ -1,39 +1,55 @@
 
-const {checkISBN} = require('./ISBN_Code')
+const { checkISBN } = require('./ISBN_Code')
 
-function checkISBN_Test(isbn)
-{
+function checkISBN_Test(isbn) {
     let res = checkISBN(isbn)
-    return res === isbn[isbn.length-1];
+    return res === isbn[isbn.length - 1];
 }
 
-describe('checkISBN',() => {
-    it('it should return false',() =>{
+describe('Correct ISBN Checksum Calculation', () => {
+    it('it should return false', () => {
         const result = checkISBN_Test("9780691147314")
         expect(result).toBe(false)
     })
-    it('should get a string',() =>{
+    it('should get a string', () => {
         const result = checkISBN_Test("9780691147314")
         expect(result).toBe(false)
     })
-    it('It should ignore "-" ',() =>{
+    it('It should ignore "-" ', () => {
         const result = checkISBN_Test("3-680-08783-7")
         expect(result).toBe(true)
     })
-    it('should allow X at last Postion and treat it as a 10',() =>{
+    it('should allow X at last Postion and treat it as a 10', () => {
         const result = checkISBN_Test("6-680-08783-X")
         expect(result).toBe(true)
     })
-    it('should allow X at last Postion and treat it as a 10',() =>{
+    it('should allow X at last Postion and treat it as a 10', () => {
         const result = checkISBN_Test("5-680-08783-X")
         expect(result).toBe(false)
     })
-    it('should return an empty string if digit is 0',() =>{
+    it('should return an empty string if digit is 0', () => {
         const result = checkISBN('147231422')
         expect(result).toBe('')
     })
-    it('should return an empty string if digit is 0',() =>{
+    it('should return an empty string if digit is 0', () => {
         const result = checkISBN('1000000001')
         expect(result).toBe("1")
+    })
+
+
+    it('should weight the first digit by one', () => {
+        expect(checkISBN('100000000')).toEqual('1')
+    })
+    it('should weight the second digit by one', () => {
+        expect(checkISBN('010000000')).toEqual('2')
+    })
+    it('should produce a "X"', () => {
+        expect(checkISBN('050000000')).toEqual('X')
+    })
+    it('should ignore dashe characters', () => {
+        expect(checkISBN('1-000-00000')).toEqual('1')
+    })
+    it('should return empty string for isbn checksum 0', () => {
+        expect(checkISBN('0-000-00000')).toEqual('')
     })
 })
